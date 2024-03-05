ccfd536d12599a08afd2cb128c06433fc6b7fafb
Eduardo Michetti <94409995+EduardoMichetti@users.noreply.github.com>
2024-03-05 09:12:37 -0300

    Tratando erros com excecao personalizada

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -12,20 +12,26 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            //Posicao P;
-            //P = new Posicao(3, 4);
-            //Console.WriteLine("Posição: " + P.Linha);
+            try
+            {
+                //Posicao P;
+                //P = new Posicao(3, 4);
+                //Console.WriteLine("Posição: " + P.Linha);
 
-            Tabuleiro tab = new Tabuleiro(8,8);
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(1, 3));
 
-            Tela.ImprimirTabuleiro(tab);
-
-
-
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+              
+           
             Console.ReadLine();
         }
     }
--- Xadrez-Console/tabuleiro/Tabuleiro.cs
+++ Xadrez-Console/tabuleiro/Tabuleiro.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace tabuleiro
 {
@@ -10,7 +11,7 @@ namespace tabuleiro
     {
         public int Linhas { get; set; }
         public int Colunas { get; set; }
-        private Peca[,] Pecas { get; set;}
+        private Peca[,] Pecas { get; set; }
 
         public Tabuleiro(int linhas, int colunas)
         {
@@ -24,10 +25,44 @@ namespace tabuleiro
             return Pecas[linha, coluna];
         }
 
+        public Peca Peca(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool ExistePeca(Posicao pos)
+        {
+            ValidarPosicao(pos);
+            return Peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (ExistePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nesta posição!");
+
+            }
             Pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool PosicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha >= Linhas ||
+                    pos.Coluna < 0 || pos.Coluna >= Colunas)
+            {
+                return false; 
+            }
+            return true;
+        }
+
+        public void ValidarPosicao(Posicao pos)
+        {
+            if(!PosicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Inválida");
+            }
         }
     }
 }
